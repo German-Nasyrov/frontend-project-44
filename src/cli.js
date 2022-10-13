9b443034752babdcccf603432edbf659648e84bc
German <german.nasyrov.95@mail.ru>
2022-10-13 12:37:55 +0300

    fix eslint errors for brain-games.js and cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,6 @@
 import readlineSync from 'readline-sync';
 
 export default function sayHello() {
-  const { method } = readlineSync;
-
   const userName = readlineSync.question('May I have your name? ');
-  console.log('Hello, ' + userName + '!');
+  console.log(`Hello, ${userName}!`);
 }
