3d025566b8f504bfcc02707e065113edf73ab65c
German <german.nasyrov.95@mail.ru>
2022-10-15 12:07:54 +0300

    the general code is placed in index.js

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import readlineSync from 'readline-sync';
+
+let successCounter = 0;
+
+export default function sayHello() {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+}
+
+export function getRandomValue() {
+  return Math.floor(Math.random() * 101);
+}
+
+export function successCheck(value, answer, rightAnswer) {
+  if (value === true) {
+    successCounter += 1;
+    console.log('Correct!');
+  } else {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+  }
+}
+
+export function result(userName) {
+  return successCounter === 3
+    ? console.log(`Congratulations, ${userName}!`)
+    : console.log(`Let's try again, ${userName}!`);
+}
