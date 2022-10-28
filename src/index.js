4e7d974e0252802750db987d1a438b9d89bb35ca
German <german.nasyrov.95@mail.ru>
2022-10-28 15:13:22 +0300

    remove magic num in loop and simplify if-else conditions

--- src/index.js
+++ src/index.js
@@ -1,31 +1,27 @@
 import readlineSync from 'readline-sync';
 
-const runLoop = (circle, description) => {
+const runLoop = (round, description) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(description);
   let successCounter = 0;
+  const roundCount = 3;
 
-  for (let i = 0; i < 3; i += 1) {
-    const [question, trueAnswer] = circle();
+  for (; successCounter < roundCount; successCounter += 1) {
+    const [question, trueAnswer] = round();
     console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
 
-    if (answer === trueAnswer) {
-      console.log('Correct!');
-      successCounter += 1;
-    } else if (answer !== trueAnswer) {
-      console.log(
-        `'${answer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.`,
-      );
-      break;
+    if (answer !== trueAnswer) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
+    console.log('Correct!');
   }
 
-  return successCounter === 3
-    ? console.log(`Congratulations, ${userName}!`)
-    : console.log(`Let's try again, ${userName}!`);
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default runLoop;
