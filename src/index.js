535099f71068c0ed1be343327772375d8b0808fc
German <german.nasyrov.95@mail.ru>
2022-11-01 08:50:52 +0300

    delete successCounter variable and remove roundCount variable from function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import readlineSync from 'readline-sync';
 
+const roundCount = 3;
+
 const runLoop = (round, description) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(description);
-  let successCounter = 0;
-  const roundCount = 3;
 
-  for (; successCounter < roundCount; successCounter += 1) {
+  for (let i = 0; i < roundCount; i += 1) {
     const [question, trueAnswer] = round();
     console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
